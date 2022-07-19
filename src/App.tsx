ae13f4722a60f66815a9be668a1276537c42b9f0
bLowther <merchanthagen@gmail.com>
2022-07-19 08:57:14 -0700

    Fixed Sript in package.json and updated server to cover for Cors, Basic start on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import { Component, ReactElement } from 'react';
 import './App.css';
-import { DefaultApi } from './swagger-generated-client';
+import { DefaultApi, Game } from './swagger-generated-client'; 
 
-export interface AppState { }
+export interface AppState {
+  id: string;
+  game: Game;
+  timer: number;
+}
 
 export class App extends Component<{}, AppState> {
   #client: DefaultApi = new DefaultApi({}, 'http://localhost:3123');
+
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      id: "0c39b11a-1123-44b5-ba74-72de7d5922fc",
+      game: {
+        completed: new Date(),
+        frames: [],
+        id: "",
+        lane: 1,
+        started: new Date(),
+      },
+      timer: 0, 
+    };
+  }
+
+  componentDidMount() {
+    this.#client.gamesGet(this.state.id)
+    .then(res => this.setState({game: res[0]}))
+    .catch(err => console.error(err))
+  }
+  
+  
   render(): ReactElement {
     return (
       <div>
-        <h1>Candlepin!</h1>
       </div>
     );
   }
