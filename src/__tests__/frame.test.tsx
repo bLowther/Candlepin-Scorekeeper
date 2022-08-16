562c5abbcfab22d022c9bc3e571b5f8af935c28c
bLowther <merchanthagen@gmail.com>
2022-08-16 16:44:23 -0700

    added frames tests

--- src/__tests__/frame.test.tsx
+++ src/__tests__/frame.test.tsx
@@ -2,14 +2,14 @@ import FrameCom from '../componets/frame';
 import { Frame } from '../core/player';
 import {render, screen} from '@testing-library/react'
 
-let testFrame = {
-  downed: [] as number[],
+let testFrame = new Frame(1,{
+  downed: [],
   completed: false,
   mark: null,
-};
+});
 
 test('Frame should contain two tiles', () => {
-  render(<FrameCom frame={new Frame(1, testFrame)} isActivePlayer={true} />);
+  render(<FrameCom frame={testFrame} isActivePlayer={true} />);
   const frame = screen.queryByTestId('frame');
   const tiles = screen.queryAllByTestId('tile');
   expect(frame).toContainElement(tiles[0]);
@@ -17,12 +17,34 @@ test('Frame should contain two tiles', () => {
 });
 
 test('First Frame of game should contain no inner html in the first tile', () => {
-  let frameProp = new Frame(1, testFrame);
-  frameProp.downed.push(4);
-  frameProp.total = frameProp.score;
+  testFrame.total = 4;
   
-  render(<FrameCom frame={frameProp} isActivePlayer={true} />);
-  const firstRender = screen.queryAllByTestId('tile')
-  expect(firstRender[0]).toBeEmptyDOMElement();
-  expect(firstRender[1]).toHaveTextContent('4');
+  render(<FrameCom frame={testFrame} isActivePlayer={true} />);
+  const tiles = screen.queryAllByTestId('tile')
+  expect(tiles[0]).toBeEmptyDOMElement();
+  expect(tiles[1]).toHaveTextContent('4');
 });
+
+test('Frame calculates scores properly', () => {
+  testFrame.number = 2;
+  testFrame.downed = [0,7,2];
+  testFrame.total = 4 + testFrame.score;
+  
+  render(<FrameCom frame={testFrame} isActivePlayer={true} />);
+  const tiles = screen.queryAllByTestId('tile');
+  expect(tiles[0]).toHaveTextContent('9');
+  expect(tiles[1]).toHaveTextContent('13');
+});
+
+test('Spares and Strikes should contain no inner html in the second tile', () => { 
+  testFrame.downed = [10];
+  testFrame.mark = 'strike'
+  testFrame.total = testFrame.score;
+  
+  render(<FrameCom frame={testFrame} isActivePlayer={true} />);
+  const tiles = screen.queryAllByTestId('tile');
+  expect(tiles[0]).toBeEmptyDOMElement();
+  expect(tiles[1]).toHaveTextContent('10');
+});
+
+
